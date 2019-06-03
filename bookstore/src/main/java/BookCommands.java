edd89271ffb491ebd8505a87f0bee42cfa7463d9
Dimitrios Vasilas <dimitrios.vasilas@scality.com>
2019-06-03 17:52:34 +0200

    add challenge/game mode

--- bookstore/src/main/java/BookCommands.java
+++ bookstore/src/main/java/BookCommands.java
@@ -1,10 +1,30 @@
 import java.util.List;
-import eu.antidotedb.client.AntidoteClient;
+import eu.antidotedb.client.*;
+import java.net.InetSocketAddress;
 
 
 public class BookCommands {
 			
-	public void getUserInfo(AntidoteClient client, String username){
+  public static String userBucket = "userbucket";
+
+  public static final RegisterKey<String> emailMapField = Key.register("email");
+  public static final SetKey<String> ownBooksMapField = Key.set("ownbooks");
+  public static final SetKey<String> borrowedBooksMapField = Key.set("borrowedbooks");
+
+  public AntidoteClient connect(String host, int port) {
+    not_implemented();
+    return null;
+  }
+
+  public void assignToRegister(AntidoteClient client, String bucket, String key, String value) {
+    not_implemented();
+  }
+
+  public void updateMapRegister(AntidoteClient client, String bucket, String key, String mapKey, String value) {
+    not_implemented();
+  }
+
+  public void getUserInfo(AntidoteClient client, String username){
 		System.out.println("User: " + username);
 		System.out.println("Email: " + getEmail(client, username));
 		System.out.println("Owned Books: " + getOwnedBooks(client, username));
